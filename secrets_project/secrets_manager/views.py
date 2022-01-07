5da2505ae5851ca950e081fb5bd6217fc9014328
Denys Zaluzhnyi <denys.zaluzhnyi@gmail.com>
2022-01-07 11:40:39 +0200

    Add template to display secrets

--- secrets_project/secrets_manager/views.py
+++ secrets_project/secrets_manager/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 
-# Create your views here.
+from .models import Secret
+
+
+class SecretCreateView(CreateView):
+    pass
+
+
+class SecretListView(ListView):
+    pass
+
+
+class SecretCreateListView(CreateView, ListView):
+    model = Secret
+    fields = ['title', 'password', 'extra']
+    context_object_name = 'secrets'
+    template_name = 'secrets_manager/secret-list.html'
+
+
+class SecretUpdateView(UpdateView):
+    pass
+
+
+class SecretDeleteView(DeleteView):
+    pass
--- secrets_project/secrets_project/settings.py
+++ secrets_project/secrets_project/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'secrets_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'statis'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
